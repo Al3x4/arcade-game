5ca8fdc5ec0d19b01de29784178f889c8574ca77
Alexandra Ralev <alexandrapredaralev@gmail.com>
2018-04-30 18:42:49 +0100

    Setup the Enemy constructor with x and y properties.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor(Math.random()*100);
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -29,7 +31,9 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+enemy1 = new Enemy(0, 0);
+const allEnemies = [];
+allEnemies.push(enemy1);
 
 
 // This listens for key presses and sends the keys to your
